fa63c0f9e4e15e0bd63e565b3153c750b5cd3019
azuhanel <87070787+falseywinchnet@users.noreply.github.com>
2025-10-14 00:16:52 -0400

    Update epistemic_AGI.py

--- epistemic_AGI.py
+++ epistemic_AGI.py
@@ -26,3 +26,29 @@ as an example: attention attempts to learn correlations between datums.
 it does not think about token sequences as a datum in and of itself,
 unless every token's embedding carries information about other tokens as markers.
 the blindest you are is to what you learned in childhood YOURSELF and take for granted as meaningful.
+
+imagine if you have conceptually embedded a bunch of concepts on a mathematical manifold.
+say a vector for each. but you only care about one dimension.
+say its fruit. lets say the least fruity fruit is a tomato, and the most fruity fruit is a lychee.
+lets say that you want to place a pomagranate somewhere in this manifold.
+because mentally, you're comparing it.
+but importantly, you have a mental map or idea already.
+you dont need to compare fruit by fruit to figure out its index.
+you know already it goes between citruses and coconuts somewhere in the "not as much fruit" section, and pretty far from berries, because it has HARD skin and SMALL CELLS OF JUICE INSIDE. its like a HARD ORANGE.
+
+but- importantly- your manifold for fruit-ness is not based on independent qualities either- sweetness, skin, texture, appearance? those are all intertwined.
+the point is- you only know points in this manifold. you learned how to enrich the manifold to add dimensions to it, *without* shifting any points in the existing dimensions. and so, you immediately *know* the point the new fruit goes at- and its nearest neighbors- simply because you know how it encodes in the manifold and you have a sort of semantic chinese remainder method.
+the question is simply this- lets translate to computer talk.
+the computer already knows similarity within a distribution.
+but, while it evolves a similar dimensional presentation, the LLM cannot simply say
+the most similar fruits on the fruityness dimension are citruses and coconuts.
+the LLM might say entirely different things because it cannot isolate those semantic manifolds easily. worse, the LLM might hallucinate that pomegranates are in the middle, 
+when clearly, melons are in the middle, and berries, dates, etc all like on the fruity end.
+apples, peaches, apricots, etc all lie just to the non-fruity side of melons.
+the human immediately approximates this. the LLM cannot.
+
+but the reason the LLM cannot- is not for a lack of knowledge.
+the LLM implicitly knows what fruits are. or rather, it knows that when thinking about fruits,
+what immediately follows are the fruits themselves. 
+it is for a lack of chinese remainder placement.
+
