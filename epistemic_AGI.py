17c01572ac04db4447fe5b3b6c953ce6111e11e7
azuhanel <87070787+falseywinchnet@users.noreply.github.com>
2025-10-14 00:03:09 -0400

    Create epistemic_AGI.py

---
+++ epistemic_AGI.py
@@ -0,0 +1,28 @@
+i will now bootstrap your idea of models into a higher thinking paradigm.
+these are the steps:
+place/frame/identify/label -> select and contextualize new DATUM or new GUESS or REFINE.
+if you dont know where you are, or your guesses dont make sense, you dont know anything.
+do one of following: retrieve familiar memory,
+or store new information.
+COMPARE familiar knowledge to new information.
+use comparison to obtain informative clues about meaning.
+store new information/digested information.
+RECURSE to achieve total meta-epistemic worldstate around semantic vectors
+contained in present reality/moment/problem and to solve any problem.
+rules used for COMPARISON are
+equivalence- nonequivalence- distance in similarity from fully equal to nothing similar-
+distance in semantic higher dimension- hash distance position
+(an operation that allows the position WITHIN a bounded space to be measured by measuring
+equivalence without measuring equivalance by search)
+
+This produces a metacognitive loop as dynamic interplay between perception, memory, and synthesis.
+It’s autoregressive in token sequence and in semantic topology.
+every new iteration refines both the ontology and the epistemic self-location within it.
+
+there is only one rule:
+DATUMS are made up of meaningful priors and components.
+an array is only a DATUM to operations that combine, not broadcast.
+as an example: attention attempts to learn correlations between datums.
+it does not think about token sequences as a datum in and of itself,
+unless every token's embedding carries information about other tokens as markers.
+the blindest you are is to what you learned in childhood YOURSELF and take for granted as meaningful.
