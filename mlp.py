1a80aca1589734142bf8b01c281d7708b5b3ee9b
azuhanel <87070787+falseywinchnet@users.noreply.github.com>
2025-12-13 08:55:32 -0500

    Update mlp.py

--- mlp.py
+++ mlp.py
@@ -1,27 +1,17 @@
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
-#optimal MLP structure.
 class MLP(nn.Module):
-    def __init__(self, config):
+    def __init__(self, d_in,h_dim,d_out):
         super().__init__()
-        HIDDEN_DIM_CONSTANT = 2 #I have code for a system that converges this dynamically to find the optimal count for asic/FPGA HW.
-        #however, in terms of times the input, 2-4x is sufficient for 99% of models and generalizes well.
-        self.c_fc    = nn.Linear( config.n_embd, HIDDEN_DIM_CONSTANT * config.n_embd, bias=config.bias)
+        self.c_fc    = nn.Linear( d_in, h_dim, bias=True)
         self.scale = math.pi / math.sqrt(3.0)
-        self.ln = LayerNorm(config.n_embd*HIDDEN_DIM_CONSTANT, bias=config.bias)
-
-        self.c_proj  = nn.Linear(HIDDEN_DIM_CONSTANT * config.n_embd, config.n_embd, bias=config.bias)
-        self.dropout = nn.Dropout(config.dropout)
+        self.c_proj  = nn.Linear(h_dim, d_out, bias=True)
 
     def forward(self, x):
         x = self.c_fc(x)
-        x = x **2 + 0.75* x**3 #todo- determine if must be adjusted for hidden_dim size changes
-        x = self.ln(x)
+        x = x **2 + 0.7049* x**3 #todo- determine if must be adjusted for hidden_dim size changes
         x = x * torch.sigmoid(self.scale * x)
         x = self.c_proj(x)
-        x = self.dropout(x)
         return x
